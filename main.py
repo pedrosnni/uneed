dffc0c360265a6dd29ec47bb5e6697f41f42e1fc
pedrosnni <p.henriquebta@gmail.com>
2023-05-12 11:48:53 -0300

    main.py 1.1

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from flask_fontawesome import FontAwesome
 import os
 
 app = Flask(__name__)
-
+fa = FontAwesome(app)
 
 @app.route('/')
 def home():
     return render_template("home.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
